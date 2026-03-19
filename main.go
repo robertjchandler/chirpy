5ab3740cd3fc832fd7e7d09373b3463dc2f5a761
robertjchandler <robertjchandler4@gmail.com>
2026-03-19 17:32:00 -0500

    update authorization

--- main.go
+++ main.go
@@ -83,6 +83,7 @@ func main() {
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUpdateUser)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -11,3 +11,9 @@ RETURNING *;
 
 -- name: GetUserByEmail :one
 SELECT * FROM users WHERE email = $1;
+
+-- name: UpdateUser :one
+UPDATE users SET email = $2,
+hashed_password = $3
+WHERE id = $1
+RETURNING *;
